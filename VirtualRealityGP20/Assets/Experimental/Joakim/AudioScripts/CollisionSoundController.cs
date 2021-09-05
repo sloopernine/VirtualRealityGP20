983868ed8ff709cb529a29b590c64cb983303dff
Joakim Stenmark <jocke.stenmark@live.se>
2021-09-05 17:26:49 +0200

    Created Collision Sound system

--- VirtualRealityGP20/Assets/Experimental/Joakim/AudioScripts/CollisionSoundController.cs
+++ VirtualRealityGP20/Assets/Experimental/Joakim/AudioScripts/CollisionSoundController.cs
@@ -6,12 +6,25 @@ public class CollisionSoundController : MonoBehaviour
 {
     public static CollisionSoundController instance;
 
-    public CollisionSoundList sounds;
-
+    //public CollisionSoundList sounds;
     
-    [SerializeField] List<AudioSource> audioSources;
+    [Tooltip("The max number of sounds (AudioSources) allowed to play. This many Audiosources are created in Awake")]
+    [SerializeField] int voicePoolSize = 24;
 
-    private AudioSource source;
+    [Tooltip("Collision sounds that will produce an impact with a volume lower than this number won't play")]
+    [SerializeField] float minCollisionVolume = 0.1f;
+    public float MinCollisionVolume { get => minCollisionVolume; set => minCollisionVolume = value; }
+    
+    [SerializeField] float maxCollisionVelocity = 5;
+    public float MaxCollisionVelocity { get => maxCollisionVelocity; set => maxCollisionVelocity = value; }
+
+    private List<AudioSource> audioSources;
+    [SerializeField] GameObject audioSourcePrefab;
+
+    private readonly string collisionSoundsPath = "CollisionSounds";
+
+    private Dictionary<CollisionSoundMaterial, List<AudioClip>> soundLibrary;
+
 
     private void Awake()
     {
@@ -25,32 +38,90 @@ public class CollisionSoundController : MonoBehaviour
             return;
         }
 
-        //DontDestroyOnLoad(gameObject);
+        audioSources = new List<AudioSource>();
 
-        source = GetComponent<AudioSource>();
+        for (int i = 0; i < voicePoolSize; i++)
+        {
+            AudioSource s = Instantiate<GameObject>(audioSourcePrefab).GetComponent<AudioSource>();
+            audioSources.Add(s);
+            s.transform.parent = this.transform;
+        }
+
+        soundLibrary = new Dictionary<CollisionSoundMaterial, List<AudioClip>>();
+        LoadSoundLibrary();
+
     }
 
-    public void Play(CollisionSoundMaterial mat)
+    private void LoadSoundLibrary()
     {
-        SoundCue_Collision cue = instance.GetCue(mat);
+        AudioClip[] clips = Resources.LoadAll<AudioClip>(collisionSoundsPath);
+        for (int index = 0; index < clips.Length; index++)
+        {
+            string name = clips[index].name;
+            int dividerIndex = name.IndexOf("__");
+            if (dividerIndex >= 0)
+                name = name.Substring(0, dividerIndex);
 
-        source.clip = cue.sounds[Random.Range(0, cue.sounds.Count)];
-        source.pitch = Random.Range(0.8f, 1.2f);
+            bool defined = System.Enum.IsDefined(typeof(CollisionSoundMaterial), name);
+
+            if (defined)
+            {
+                CollisionSoundMaterial mat = (CollisionSoundMaterial)System.Enum.Parse(typeof(CollisionSoundMaterial), name);
+                if (soundLibrary.ContainsKey(mat) == false || soundLibrary[mat] == null)
+                {
+                    soundLibrary[mat] = new List<AudioClip>();
+                }
+                soundLibrary[mat].Add(clips[index]);
+
+            }
+            else
+            {
+                Debug.LogWarning("[SoundSystem] CollisionSound: Found clip for material that is not in enumeration: " + clips[index].name);
+            }
+        }
+    }
+
+    public void Play(CollisionSoundMaterial mat, Vector3 position, float impactVolume = 1f)
+    {
+        if (!soundLibrary.ContainsKey(mat))
+        {
+            Debug.LogError("[SoundSystem] CollisionSound: Trying to play sound for material without a clip. Need a clip at: " + collisionSoundsPath + "/" + mat.ToString());
+            mat = CollisionSoundMaterial._default;
+            return;
+        }
+
+        AudioSource source = GetIdleAudioSource();
+        source.clip = soundLibrary[mat][Random.Range(0, soundLibrary[mat].Count)];
+        source.transform.position = position;
+        source.pitch = Random.Range(0.7f, 1.3f);
         source.Play();
 
-
     }
-
-
-    private SoundCue_Collision GetCue(CollisionSoundMaterial mat)
+    private AudioSource GetIdleAudioSource()
     {
-        SoundCue_Collision cue = sounds.soundList.Find(x => x.material == mat);
-        if (cue == null)
+        if (audioSources.Exists(source => !source.isPlaying))
         {
-            Debug.LogWarning($"No Cue set for {mat}");
-            return null;
+            return audioSources.Find(source => !source.isPlaying);
         }
-        return cue;
+        AudioSource s = Instantiate<GameObject>(audioSourcePrefab).GetComponent<AudioSource>();
+        audioSources.Add(s);
+        Debug.LogWarning("[SoundSystem] CollisionSound: No idle audioSource found, adding new one. Consider increasing VoicePoolSize");
+
+        return s;
+
     }
+
+
+    //old system
+    //private SoundCue_Collision GetCue(CollisionSoundMaterial mat)
+    //{
+    //    SoundCue_Collision cue = sounds.soundList.Find(x => x.material == mat);
+    //    if (cue == null)
+    //    {
+    //        Debug.LogWarning($"No Cue set for {mat}");
+    //        return null;
+    //    }
+    //    return cue;
+    //}
 
 }
