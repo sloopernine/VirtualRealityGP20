48ed3a7c0e3fc5b4825925bcc5039eebcb4f14a1
sloopernine <robin@omnick.se>
2021-09-06 15:27:44 +0200

    fixed steam input

--- VirtualRealityGP20/Assets/Experimental/Robin/RuneMaker.cs
+++ VirtualRealityGP20/Assets/Experimental/Robin/RuneMaker.cs
@@ -1,46 +1,44 @@
-using System;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.XR;
-using UnityEngine.XR.Interaction.Toolkit;
 using PDollarGestureRecognizer;
 using System.IO;
+using Valve.VR;
 
 public class RuneMaker : MonoBehaviour
 {
     private LineRenderer lineRenderer;
+    private bool isMoving;
     
-    public XRNode inputSource;
-    public InputHelpers.Button inputButton;
-    public float inputThreshold = 0.1f;
+    public SteamVR_Input_Sources handType;
+    public SteamVR_Action_Boolean grabAction;
 
     private bool isMoving;
-
+    
     public GameObject debugPrefab;
     public bool trainingMode;
     public string newGestureName;
     private List<Gesture> trainingSet = new List<Gesture>();
-
+    
     public Transform trackedTransform;
     public float newPositionThresholdDistance = 0.1f;
     public List<Vector3> pointCloudList = new List<Vector3>();
-
+    
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
-
+        
         string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
         foreach (var item in gestureFiles)
         {
             trainingSet.Add(GestureIO.ReadGestureFromFile(item));
         }
     }
-
+    
     private void Update()
     {
-        InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
-
+        bool isPressed = grabAction.GetState(handType);
+        
         Vector3 trackedPosition = trackedTransform.position;
         
         if (!isMoving && isPressed)
@@ -56,7 +54,7 @@ public class RuneMaker : MonoBehaviour
             UpdateMovement(trackedPosition);
         }
     }
-
+    
     private void StartMovement(Vector3 position)
     {
         isMoving = true;
@@ -66,13 +64,13 @@ public class RuneMaker : MonoBehaviour
         lineRenderer.SetPosition(0, position);
         CreateDebugCube(position);
     }
-
+    
     private void EndMovement(Vector3 position)
     {
         isMoving = false;
-
+    
         Point[] pointArray = new Point[pointCloudList.Count];
-
+        
         for (int i = 0; i < pointArray.Length; i++)
         {
             Vector2 screenPoint = Camera.main.WorldToScreenPoint(pointCloudList[i]);
@@ -80,12 +78,12 @@ public class RuneMaker : MonoBehaviour
         }
 
         Gesture newGesture = new Gesture(pointArray);
-
+        
         if (trainingMode)
         {
             newGesture.Name = newGestureName;
             trainingSet.Add(newGesture);
-
+            
             string path = Application.persistentDataPath + "/" + newGestureName + ".xml";
             GestureIO.WriteGesture(pointArray, newGestureName, path);
         }
@@ -95,11 +93,11 @@ public class RuneMaker : MonoBehaviour
             Debug.Log(result.GestureClass + result.Score);
         }
     }
-
+    
     private void UpdateMovement(Vector3 position)
     {
         Vector3 lastPoint = pointCloudList[pointCloudList.Count - 1];
-
+        
         //Debug.Log("Updating movement: " + Vector3.Distance(pointSource.position, lastPoint));
         
         if (Vector3.Distance(trackedTransform.position, lastPoint) > newPositionThresholdDistance)
@@ -115,7 +113,7 @@ public class RuneMaker : MonoBehaviour
             lineRenderer.SetPosition(pointCloudList.Count - 1, position);
         }
     }
-
+    
     private void CreateDebugCube(Vector3 position)
     {
         return;
