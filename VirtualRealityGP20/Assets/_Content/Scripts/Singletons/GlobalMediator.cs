5e2535828b219e2c6bd4982ed11e49fe7a2b6fa1
sloopernine <robin@omnick.se>
2021-09-07 10:08:08 +0200

    global mediator

--- VirtualRealityGP20/Assets/_Content/Scripts/Singletons/GlobalMediator.cs
+++ VirtualRealityGP20/Assets/_Content/Scripts/Singletons/GlobalMediator.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using _Content.Scripts.Data.Containers.GlobalSignal;
 using Data.Interfaces;
 using UnityEngine;
@@ -8,14 +9,43 @@ namespace _Content.Scripts.Singletons
     [DefaultExecutionOrder(-9)]
     public class GlobalMediator : MonoBehaviour, IReceiveGlobalSignal, ISendGlobalSignal
     {
+        public static GlobalMediator Instance;
+        
+        private List<IReceiveGlobalSignal> subscribers = new List<IReceiveGlobalSignal>();
+        
+        public int totalSubscribers;
+        
+        private void Awake()
+        {
+            if (Instance == null)
+                Instance = this;
+            else
+                Destroy(gameObject);
+        }
+        
         public void ReceiveGlobal(Enum eventState, GlobalSignalBaseData globalSignalData = null)
         {
-            throw new NotImplementedException();
+            SendGlobal(eventState, globalSignalData);
         }
 
-        public void SendGlobal(Enum eventState, GlobalSignalBaseData globalSignalData = null)
+        public void SendGlobal(Enum eventState, GlobalSignalBaseData signalData)
         {
-            throw new NotImplementedException();
+            foreach (var subscriber in subscribers)
+            {
+                subscriber.ReceiveGlobal(eventState, signalData);
+            }
+        }
+
+        public void Subscribe(IReceiveGlobalSignal subscriber)
+        {
+            subscribers.Add(subscriber);
+            totalSubscribers++;
+        }
+
+        public void UnSubscribe(IReceiveGlobalSignal subscriber)
+        {
+            subscribers.Remove(subscriber);
+            totalSubscribers--;
         }
     }
 }
